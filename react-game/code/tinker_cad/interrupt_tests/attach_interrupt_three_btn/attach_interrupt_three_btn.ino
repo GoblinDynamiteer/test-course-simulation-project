5646c4cef8371f350e82d2cc3222a96526fc4f74
Johan Kämpe <kampe.johan@gmail.com>
2017-12-08 13:37:42 +0100

    TinkerCad 3btn int test

---
+++ react-game/code/tinker_cad/interrupt_tests/attach_interrupt_three_btn/attach_interrupt_three_btn.ino
@@ -0,0 +1,65 @@
+#define INT_PIN 3
+#define BUTTON_STATUS_DOWN 0
+#define ERROR -1
+
+enum { RED, GREEN, BLUE, COLOR_MAX, BTN_MAX = COLOR_MAX };
+const int rgb_led_pin[] = { 10, 11, 12 };
+const int rgb_btn_pin[] = { 4, 5, 6 };
+
+bool button_pressed;
+
+void setup()
+{
+    for(int i = 0; i < 3; i++)
+    {
+        pinMode(rgb_led_pin[i], OUTPUT);
+    }
+
+    pinMode(INT_PIN, INPUT_PULLUP);
+    attachInterrupt(
+        digitalPinToInterrupt(INT_PIN), isr_button_click, FALLING);
+
+    button_pressed = false;
+    Serial.begin(9600);
+}
+
+void loop()
+{
+    if(button_pressed)
+    {
+        int color = get_pressed_button();
+        Serial.println("Setting color: " + String(color));
+        set_rgb_led(color);
+        button_pressed = false;
+    }
+
+    delay(1);
+}
+
+void isr_button_click(void)
+{
+    button_pressed = true;
+}
+
+int get_pressed_button(void)
+{
+    for(int i = 0; i < BTN_MAX; i++)
+    {
+        if(digitalRead(rgb_led_pin[i]) == BUTTON_STATUS_DOWN)
+        {
+            return i;
+        }
+    }
+
+    return ERROR;
+}
+
+void set_rgb_led(int color)
+{
+    for(int i = 0; i < COLOR_MAX; i++)
+    {
+        digitalWrite(rgb_led_pin[i], LOW);
+    }
+
+    digitalWrite(rgb_led_pin[color], HIGH);
+}
